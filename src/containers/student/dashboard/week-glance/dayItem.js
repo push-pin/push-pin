61a33c882cd2b8f4ef58c91203931d93a0159425
Anna Ridenour <annamridenour@gmail.com>
2019-06-13 10:55:59 -0700

    made dayItem component

--- src/containers/student/dashboard/week-glance/dayItem.js
+++ src/containers/student/dashboard/week-glance/dayItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function dayItem({ type, title, dueDate }) {
+function dayItem({ type, title, dueDate, submitted }) {
   return (
     <>
       <h3>{type}</h3>
       <h4>{title}</h4>
       <p>{dueDate}</p>
+      <p>Submitted: {submitted}</p>
     </>
   );
 }
@@ -14,7 +15,10 @@ function dayItem({ type, title, dueDate }) {
 dayItem.propTypes = {
   type: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  dueDate: PropTypes.string.isRequired
+  dueDate: PropTypes.string,
+  submitted: PropTypes.bool.isRequired
 };
+
+//completed is a boolean, will eventually be represented as checkmark, color, something along those lines
 
 export default dayItem;
