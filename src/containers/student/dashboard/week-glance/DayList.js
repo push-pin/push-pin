95a10296c0013e53d52988c8b2a3e7b39f270516
Anna Ridenour <annamridenour@gmail.com>
2019-06-13 11:25:50 -0700

    wrote WeekList component

--- src/containers/student/dashboard/week-glance/DayList.js
+++ src/containers/student/dashboard/week-glance/DayList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import DayItem from './dayItem';
 
-function DayList({ dayArray, day, date }) {
+function DayList({ dayArray }) {
   const dayItems = dayArray.map(item => {
     return (
       <li key={item._id}>
@@ -12,20 +12,16 @@ function DayList({ dayArray, day, date }) {
   });
 
   return (
-    <li>
-      <h2>{day}</h2>
-      <h3>{date}</h3>
+    <>
       <ul>
         {dayItems}
       </ul>
-    </li>
+    </>
   );
 }
 
 DayList.propTypes = {
   dayArray: PropTypes.array.isRequired,
-  day: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired
 };
 
 export default DayList;
