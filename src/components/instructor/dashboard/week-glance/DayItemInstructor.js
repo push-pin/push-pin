f990b7b2b75c557a3d2bf8afcbfbab2e27c72066
Anna Ridenour <annamridenour@gmail.com>
2019-06-14 13:14:29 -0700

    initial commit

--- src/components/instructor/dashboard/week-glance/DayItemInstructor.js
+++ src/components/instructor/dashboard/week-glance/DayItemInstructor.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function DayItem({ type, title, dueDate, submitted }) {
+function DayItem({ type, title, dueDate, graded }) {
   return (
     <>
       <h3>{type}</h3>
       <h4>{title}</h4>
       <p>{dueDate}</p>
-      <p>Submitted: {submitted}</p>
+      <p>Graded: {submitted}</p> 
+      //this is where we want how many are graded
     </>
   );
 }
-
-//don't forget to pass id for clickability
 
 DayItem.propTypes = {
   type: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   dueDate: PropTypes.string,
-  submitted: PropTypes.bool.isRequired
+  graded: PropTypes.bool.isRequired
 };
 
 //completed is a boolean, will eventually be represented as checkmark, color, something along those lines
--- src/containers/student/dashboard/WeekAtGlance.js
+++ src/containers/student/dashboard/WeekAtGlance.js
@@ -1,6 +1,57 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import WeekList from '../../../components/student/dashboard/week-glance/WeekList';
+import { connect } from 'react-redux';
+import { getWeek, getWeekError, getWeekLoading } from '../../../selectors/student/dashboard/weekSelectors';
+import { getWeekAtGlance } from '../../../actions/student/dashboard/weekActions';
 
 class WeekContainer extends PureComponent {
-  
+  static propTypes = {
+    fetch: PropTypes.func.isRequired,
+    assignments: PropTypes.object.isRequired,
+    loading: PropTypes.bool.isRequired, 
+    error: PropTypes.string.isRequired
+  }
+
+  componentDidMount() {
+    console.log('mounty');
+    this.props.fetch();
+  }
+
+  render(){
+    console.log('render');
+    console.log(this.props.loading);
+    // return <h1>week</h1>;
+    if(!this.props.assignments.Mon) {  //change back to loading once fetch is a promise
+      return <h1>Loading</h1>;
+    }
+    else {
+      return (
+        <section>
+          <h1>Week At Glance</h1>
+          <WeekList assignments={this.props.assignments} />
+        </section>
+      );
+    }
+  }
 }
+
+const mapStateToProps = state => ({
+  assignments: getWeek(state),
+  loading: getWeekLoading(state),
+  error: getWeekError(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetch() {
+    console.log('in dispatch fetch');
+    dispatch(getWeekAtGlance());
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(WeekContainer);
+
+
