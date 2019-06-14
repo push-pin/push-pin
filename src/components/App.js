9461708b9d407628229a15136cb43776ad1d936e
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-06-14 13:30:57 -0700

    layout

--- src/components/App.js
+++ src/components/App.js
@@ -8,15 +8,18 @@ import Home from './Home';
 import Callback from '../containers/auth/Callback';
 import { withSession } from '../containers/auth/withSession';
 import NavContainer from '../containers/navbar/NavContainer';
+import Styles from './App.css';
 
 export default function App() {
   return (
-    <Router>
-      <NavContainer />
-      <Switch>
-        <Route exact path="/" component={withSession(Home)} />
-        <Route path="/callback" component={Callback} />
-      </Switch>
-    </Router>
+    <section className={Styles.App}>
+      <Router>
+        <NavContainer />
+        <Switch>
+          <Route exact path="/" component={withSession(Home)} />
+          <Route path="/callback" component={Callback} />
+        </Switch>
+      </Router>
+    </section>
   );
 }
