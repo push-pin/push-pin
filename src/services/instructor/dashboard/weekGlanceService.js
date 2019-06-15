44d79a720d7204ef1efebe65151de893f8d87b82
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-06-15 14:10:22 -0700

    working on instructor week at a glance

--- src/services/instructor/dashboard/weekGlanceService.js
+++ src/services/instructor/dashboard/weekGlanceService.js
@@ -1,4 +1,4 @@
-export function getInstWeek() {
+export function fetchInstWeek() {
   const today = new Date();
   const diff = today.getDate() - today.getDay() + (today.getDay() === 0 ? -6 : 1);
   const monday =  new Date(today.setDate(diff));
