ed62b499c6551e373771755f66511630253ea54f
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-06-16 12:24:08 -0700

    instructor actions tests

---
+++ src/actions/instructor/dashboard/recentSubsActions.test.js
@@ -0,0 +1,16 @@
+import { getRecentSubs } from './recentSubsActions';
+
+describe('instructor recent submissions actions', () => {
+  it('creates an action to fetch week', () => {
+    const action = getRecentSubs();
+
+    expect(action).toEqual({
+      type: 'GET_RECENT_SUBS',
+      pendingType: 'GET_RECENT_SUBS_PENDING',
+      fulfilledType: 'GET_RECENT_SUBS_FULFILLED',
+      rejectedType: 'GET_RECENT_SUBS_REJECTED',
+      payload: expect.any(Array)
+      //payload will become a promise when we switch to real fetches
+    });
+  });
+});
