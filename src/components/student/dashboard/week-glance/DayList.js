9e2850e66ffd9e2d043b301d925b2c86d6d6dc1a
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-06-17 09:50:28 -0700

    fixing import

--- src/components/student/dashboard/week-glance/DayList.js
+++ src/components/student/dashboard/week-glance/DayList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import DayItem from './dayItem';
+import DayItem from './DayItem';
 import { ListItemWeek, ListItemUlWeek } from '../../../../../styles/components/ListItem';
 
 function DayList({ dayArray }) {
