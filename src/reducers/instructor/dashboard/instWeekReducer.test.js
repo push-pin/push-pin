213a324a366ccdc63c54e2b153ca186bb860c92c
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-06-16 21:28:26 -0700

    finished tests

--- src/reducers/instructor/dashboard/instWeekReducer.test.js
+++ src/reducers/instructor/dashboard/instWeekReducer.test.js
@@ -16,4 +16,26 @@ describe('instructor week at a glance reducer', () => {
       error: {}
     });
   });
+  it('handles the GET_INST_WEEK action', () => {
+    const newState = reducer(initialState, {
+      type: 'GET_INST_WEEK',
+      payload: expect.any(Object)
+    });
+    expect(newState).toEqual({
+      assignments: expect.any(Object),
+      error: {},
+      loading: false
+    });
+  });
+  it('handles the GET_INST_WEEK_ERROR action', () => {
+    const newState = reducer(initialState, {
+      type: 'GET_INST_WEEK_ERROR',
+      payload: expect.any(Object)
+    });
+    expect(newState).toEqual({
+      assignments: {},
+      loading: false,
+      error: expect.any(Object)
+    });
+  });
 });
