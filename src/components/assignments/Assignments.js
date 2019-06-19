7422cbdfa8a25a5c3772ef2bc977cdda07644fbc
UncleJerry <jared.myhrberg@gmail.com>
2019-06-19 16:29:48 -0700

    assignments written

---
+++ src/components/assignments/Assignments.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Assignment from './Assignment';
+
+function Assignments({ assignments }) {
+  const assignmentListItems = assignments.map(ass => {
+    return (
+      <li key={ass._id}>
+        <Assignment assignment={ass} />
+      </li>
+    );
+  });
+
+  return (
+    <ul>
+      {assignmentListItems}
+    </ul>
+  );
+}
+
+Assignments.propTypes = {
+  assignments: PropTypes.array.isRequired
+};
