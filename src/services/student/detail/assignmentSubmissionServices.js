1c66552a09891aff29174debea1949d352e4f42e
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-06-18 21:13:58 -0700

    switching branches

--- src/services/student/detail/assignmentSubmissionServices.js
+++ src/services/student/detail/assignmentSubmissionServices.js
@@ -1,9 +1,9 @@
 import { post, get, put } from '../../request';
 
 export const postSubmission = (assignment, student, submission) => {
-  return post('/api/v1/submissions', { assignment, student, submission });
+  return post('api/v1/submissions', { assignment, student, submission });
 };
 
 export const getAssignmentById = assignmentId => {
-  return get(`/api/v1/assignments/${assignmentId}`);
+  return get(`api/v1/assignments/${assignmentId}`);
 };
