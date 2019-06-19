5b9bfd0ea1c7980fdf164b0f2318f3fa07f04c64
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-06-19 12:59:11 -0700

    updates for week at glance

--- src/components/student/dashboard/week-glance/DayItem.js
+++ src/components/student/dashboard/week-glance/DayItem.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import { LinkStyled } from '../../../../../styles/components/LinkStyled';
 
 function DayItem({ type, title, dateDue, submitted, _id }) {
