e1130ca4b133ddf3f51543de274df82cb4829002
UncleJerry <jared.myhrberg@gmail.com>
2019-06-19 14:26:39 -0700

    setting up assignments view

---
+++ src/reducers/assignment/assReducer.js
@@ -0,0 +1,20 @@
+import { GET_ASSES_BY_COURSE, GET_ASSES_BY_COURSE_PENDING, GET_ASSES_BY_COURSE_ERROR } from '../../../src/actions/assignment/assActions';
+
+const initialState = {
+  asses: [],
+  subs:[],
+  loading: false
+};
+
+export default function reducer(state = initialState, action) {
+  switch(action.type) {
+    case GET_ASSES_BY_COURSE:
+      return { ...state, asses: action.payload.asses, subs: action.payload.subs, loading: false };
+    case GET_ASSES_BY_COURSE_PENDING:
+      return { ...state, loading: true };
+    case GET_ASSES_BY_COURSE_ERROR:
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+}
---
+++ src/selectors/assignment/assSelectors.js
@@ -0,0 +1,2 @@
+export const selectAssesByCourse = state => state.courseAsses.asses;
+export const selectSubsByCourse = state => state.courseAsses.subs;
