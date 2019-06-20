a9d67184ef026fc8d75073a0ab3b5d0b2bd1db8e
Anna Ridenour <annamridenour@gmail.com>
2019-06-20 10:16:02 -0700

    moved utils back to where it was (oops)

---
+++ src/utils/SampleDataFunctions.js
@@ -0,0 +1,9 @@
+import { post, get, put } from '../services/request';
+
+function createAssignment(course, type, title, instructions, classDate, dateAvailable, dateDue, dateClosed) {
+  return post('/api/v1/assignments', { course, type, title, instructions, classDate, dateAvailable, dateDue, dateClosed });
+};
+
+createAssignment(
+  '5d0bb7e32fd069001762b8c0'
+)
