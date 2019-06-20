103a272f0b21ec5acccd8220ae056cc4f1bc08ff
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-06-19 22:29:18 -0700

    finished time/date formatter

--- src/components/student/dashboard/announcements/Announcement.js
+++ src/components/student/dashboard/announcements/Announcement.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { LinkStyled } from '../../../../../styles/components/LinkStyled';
 import formattedDate from '../../../../utils/date-formatter';
 import abridgedText from '../../../../utils/text-abridge';
 
+
 function Announcement({ title, body, id, updatedAt }) {
-  
+
   const date = formattedDate(updatedAt);
   const shortBody = abridgedText(body, 20);
 
   return (
     <> 
-     {/*make this a link to mark as read and increment unread counter  */}
-      <div className="date">
-        <h3>{date.month}/{date.day}/{date.year} :: {date.time}</h3>
-      </div>
-      <div className="info">
-        <h4>{title}</h4>
-        <p className="preview">{shortBody}</p>
-        <p>{id}</p>  {/*delete this later once link is created */}
-      </div>
+     {/*make this a link to mark as read and increment unread counter - how will we track whether a specific user has read an announcement? */}
+     {/* <LinkStyled > */}
+       <div className="date">
+         <h3>{date.month}/{date.day}/{date.year} :: {date.time}</h3>
+       </div>
+       <div className="info">
+         <h4>{title}</h4>
+         <p className="preview">{shortBody}</p>
+         <p>{id}</p>  {/*delete this later once link is created */}
+       </div>
+     {/* </LinkStyled> */}
     </>
   );
 }
