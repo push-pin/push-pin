a15821fd4053dd895f093396cb7ba6b951f87230
Anna Ridenour <annamridenour@gmail.com>
2019-06-20 10:02:04 -0700

    hopefully not messing anything up

---
+++ styles/components/SubmissionFields.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const SubmitField = styled.section `
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 25rem;
+  background-color: #FDFDFD;
+  padding-bottom: 5rem;
+   div:nth-child(2) {
+     /* border: 2px solid green; */
+     height: 10rem;
+   }
+`;
+
+export { SubmitField };
