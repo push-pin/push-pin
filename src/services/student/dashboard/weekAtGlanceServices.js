582e832baf925db1b29aff7a28bf46390c40f777
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-06-21 09:54:53 -0700

    fixing service

--- src/services/student/dashboard/weekAtGlanceServices.js
+++ src/services/student/dashboard/weekAtGlanceServices.js
@@ -1,6 +1,5 @@
 import { get } from '../../request';
 
 export const fetchWeekAtGlance = (userId, courseId) => {
-  // return get(`/api/v1/assignments/weekataglance/course/5d0c50e51136040017b428fa/student/${userId}`);
   return get(`/api/v1/assignments/weekataglance/course/${courseId}/student/${userId}`);
 };
